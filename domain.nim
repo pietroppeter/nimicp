c555ed0f52e1bdc9b2ed0c26c179d63426b407e0
Pietro Peterlongo <pietro.peterlongo@agilelab.com>
2024-10-19 11:04:50 +0200

    potential bug in concept (or in my concept undrestanding)

--- domain.nim
+++ domain.nim
@@ -1,15 +1,32 @@
-# implementation of a sparseset data structure
-# used for domain
+# domain concept (will be implemented as a sparset)
 
 type
   IntDomain* = concept d
     d.min is int  
     d.max is int
-    d.size is int
+    #d.size is int
     # d.contains(v: int) is bool
     # d.isBound is bool # if there is only a single value
     # e.remove(v: int, l: DomainListener) # e is a var IntDomain
     # e.removeAllBut(v: int, l: DomainListener)
     # e.removeBelow(v: int, l: DomainListener)
     # e.removeAbove(v: int, l: DomainListener)
-    $(d) is string
+    #$(d) is string
+
+type
+  IntDomainAsSeq = seq[int]
+
+var d: IntDomainAsSeq = @[1, 2, 3]
+
+echo d.min(), d.max()
+
+# to test concept
+func span*(d: IntDomain): int =
+  d.max - d.min
+
+echo d.span()
+#[ why error?
+Error: internal error: openArrayLoc: IntDomain
+No stack traceback available
+
+]#
