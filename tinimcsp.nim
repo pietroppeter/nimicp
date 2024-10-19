e72d3eace2b649c734ecf4efa7203b7cf0fe8f69
Pietro Peterlongo <pietro.peterlongo@agilelab.com>
2024-10-19 11:45:34 +0200

    stuck on min not available

--- tinimcsp.nim
+++ tinimcsp.nim
@@ -5,9 +5,18 @@ type
   Domain* = object
     values: IntSet
 
-func newDomain(n: int): Domain =
+func newDomain*(n: int): Domain =
   result.values = (0 ..< n).toSeq.toIntSet
+
+func size*(d: Domain): int =
+  d.values.len
+
+func min*(d: Domain): int =
+  discard
+  #d.values.min # min not available for intsets!
+
 
 when isMainModule:
   var d = newDomain(3)
   echo d
+  echo d.size
